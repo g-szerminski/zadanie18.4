9fa1a7359723ea0eb6ae9f03f33c72d96455edb6
Grzegorz Szermiński <szerman@poczta.fm>
2018-04-16 18:21:58 +0200

    Add index.js content

--- index.js
+++ index.js
@@ -13,9 +13,40 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket) {
-	//miejsce dla funkcji, które zostaną wykonane po podłączeniu klienta
+// klient nasłuchuje na wiadomość wejścia do czatu
+	socket.on('join', function(name) {
+	// użytkownika, który pojawił się w aplikacji zapisujemy do serwisu trzymającego listę osób w czacie
+		userService.addUser({
+			id: socket.id,
+			name
+		});
+// aplikacja emituje zdarzenie update, które aktualizuje informację na temat listy użytkowników każdemu nasłuchującemu na wydarzenie 'update'
+		io.emit('update', {
+			users: userService.getAllUsers()
+ 		});
+	});
+//miejsce dla funkcji, które zostaną wykonane po podłączeniu klienta
 });
 
-server.listen(3000, function(){
+io.on('connection', function(socket) {
+	socket.on('disconnect', () => {
+		userService.removeUser(socket.id);
+		socket.broadcast.emit('update', {
+			users: userService.getAllUsers()
+		});
+	});
+});
+
+io.on('connection', function(socket) {
+	socket.on('message', function(message){
+		const {name} = userService.getUserById(socket.id);
+		socket.broadcast.emit('message', {
+			text: message.text,
+			from: name
+		});
+	});
+});
+
+server.listen(3000, function() {
 	console.log('listening on *:3000');
 });
